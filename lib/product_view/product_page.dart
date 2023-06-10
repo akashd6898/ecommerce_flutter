dce8c63c357dc64e06ad18260af807a302c65c8c
akashd6898 <akashd.aug06@gmail.com>
2023-06-10 09:55:08 +0530

    updated Product_Page visuals.

--- lib/product_view/product_page.dart
+++ lib/product_view/product_page.dart
@@ -15,25 +15,65 @@ class _ProductPageState extends State<ProductPage> {
       appBar: AppBar(
         title: const Text("Product"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 350,
-            width: double.infinity,
-            child: PageView.builder(itemCount: PhoneList.itemsList[0].imageSet?.length,
-                itemBuilder: (context,index)
-            {
-              return Image(image: NetworkImage(PhoneList.itemsList[0].imageSet![index]),
-                  fit: BoxFit.fill,
-                  width: 300,
-                  height: 200,);
-            }),
-          ),
-          Text(PhoneList.itemsList[0].description),
-        ],
-      ),
-    );
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Brand: ${PhoneList.itemsList[0].brand}",
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+            ),
+            Text("Model: ${PhoneList.itemsList[0].model}"),
+            SizedBox(
+              height: 350,
+              width: double.infinity,
+              child: PageView.builder(itemCount: PhoneList.itemsList[0].imageSet?.length,
+                  itemBuilder: (context,index)
+              {
+                return Image(image: NetworkImage(PhoneList.itemsList[0].imageSet![index]),
+                    fit: BoxFit.fill,
+                    width: 300,
+                    height: 200,);
+              }),
+            ),
+            Text("Price: ₹${PhoneList.itemsList[0].price}"),
+            Text(PhoneList.itemsList[0].description),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children:[
+                ElevatedButton(onPressed: null,
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.blue
+                        ),
+                      fixedSize: MaterialStateProperty.all<Size>(const Size(150, 50)),
+                      elevation: MaterialStateProperty.all(5)
+                    ),
+                    child:
+                        const Icon(
+                        Icons.add_shopping_cart,
+                            color: Colors.white
+                        )),
+                 ElevatedButton(onPressed: null,
+                     style: ButtonStyle(
+                         backgroundColor: MaterialStateProperty.all<Color>(
+                             Colors.blue
+                         ),
+                         fixedSize: MaterialStateProperty.all<Size>(const Size(150, 50)),
+                         elevation: MaterialStateProperty.all(5)
+                     ),
+                     child:
+                     const Icon(
+                         Icons.favorite,
+                         color: Colors.white
+                     ))]
+             )],
+            ),
+      )
+      );
   }
 }
 
