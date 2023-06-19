e6bb75f03d8888e7630bbd0d34a53dc32b65dbf0
akashd6898 <akashd.aug06@gmail.com>
2023-06-19 22:46:44 +0530

    updated checkout page visual with order summary and Pay button.

--- lib/checkout_page/checkout_page.dart
+++ lib/checkout_page/checkout_page.dart
@@ -31,8 +31,10 @@ class _CheckoutPageState extends State<CheckoutPage> {
                 Container(
                   padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
                   width: 350,
-                  height: 450,
-                  color: Colors.white70,
+                  height: 400,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
                   child: CustomScrollView(
                     scrollDirection: Axis.vertical,
                     slivers: [
@@ -42,7 +44,13 @@ class _CheckoutPageState extends State<CheckoutPage> {
                             child: Container(
                             width: 250,
                             height: 80,
-                            color: Colors.white,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                              offset: const Offset(0, 3),
+                              )]
+                            ),
                               child: Row(
                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 mainAxisSize: MainAxisSize.max,
@@ -69,9 +77,140 @@ class _CheckoutPageState extends State<CheckoutPage> {
                       )
                     ],
                   )
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  height: 300,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade300,
+                    //gradient: LinearGradient(colors: [Colors.white70, Colors.lightBlueAccent])
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [const Text("Order Summary",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [Text("Sub-Total:",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),
+                        strutStyle: StrutStyle(
+                          height: 3
+                        ),),
+                          Text("1000",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                            ),
+                            strutStyle: StrutStyle(
+                                height: 3
+                            ),)
+                      ]),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [Text("Tax:",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                            )),
+                          Text("1000",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                            ),
+                            strutStyle: StrutStyle(
+                                height: 3
+                            ),)
+                        ]
+                      ),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [Text("Delivery charge:",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                            )),
+                          Text("1000",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                            ),
+                            strutStyle: StrutStyle(
+                                height: 3
+                            ),)
+                        ]
+                      ),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [Text("Total:",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
+                          strutStyle: StrutStyle(
+                            height: 3,
+                          ),
+                        ),
+                          Text("1000",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold
+                            ),
+                            strutStyle: StrutStyle(
+                                height: 3
+                            ),)
+                        ]
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                        Container(
+                          padding: const EdgeInsets.all(5),
+                          height: 40,
+                          width: 100,
+                          alignment: Alignment.center,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadiusDirectional.all(Radius.circular(7)),
+                            gradient: LinearGradient(colors: [Colors.cyanAccent,Colors.lightBlueAccent, Colors.blue]),
+                            boxShadow: [BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0, 3)
+                            )]
+                          ),
+                          child: const Text(
+                            "Pay",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        )
+                      ],)
+                    ],
+                  ),
                 )
-          ])
-      )
+              ],
+          )
+      ),
     );
   }
 }
